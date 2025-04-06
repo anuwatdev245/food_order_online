9646ec2b5517900b18e8071e3d79e16c10291b95
anuwat <tox.anw.6114@gmail.com>
2025-04-06 13:31:02 +0700

    commit

--- src/main/java/com/anuwat/food_order_online/model/Order.java
+++ src/main/java/com/anuwat/food_order_online/model/Order.java
@@ -1,7 +1,15 @@
 package com.anuwat.food_order_online.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
 }
