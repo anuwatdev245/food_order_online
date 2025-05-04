c46766724ae6f2dc16869be63ca05d89cb63ce14
anuwatdev245 <tox.anw.6114@gmail.com>
2025-05-04 12:54:57 +0700

    commit

--- src/main/java/com/anuwat/food_order_online/service/CustomerUserDetailsService.java
+++ src/main/java/com/anuwat/food_order_online/service/CustomerUserDetailsService.java
@@ -1,11 +1,17 @@
 package com.anuwat.food_order_online.service;
 
+import com.anuwat.food_order_online.model.USER_ROLE;
 import com.anuwat.food_order_online.model.User;
 import com.anuwat.food_order_online.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CustomerUserDetailsService implements UserDetailsService {
@@ -25,6 +31,11 @@ public class CustomerUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("user not found with email "+username);
         }
 
-        return null;
+        USER_ROLE role = user.getRole();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(role.toString()));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
     }
 }
