3673599b7dbd10f646b310ebd649b82bfcefdcfa
anuwatdev245 <tox.anw.6114@gmail.com>
2025-05-06 17:35:19 +0700

    commit

--- src/main/java/com/anuwat/food_order_online/controller/AuthController.java
+++ src/main/java/com/anuwat/food_order_online/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.anuwat.food_order_online.controller;
 
 import com.anuwat.food_order_online.config.JwtProvider;
+import com.anuwat.food_order_online.model.Cart;
+import com.anuwat.food_order_online.model.User;
 import com.anuwat.food_order_online.repository.CartRepository;
 import com.anuwat.food_order_online.repository.UserRepository;
+import com.anuwat.food_order_online.response.AuthResponse;
 import com.anuwat.food_order_online.service.CustomerUserDetailsService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,31 @@ public class AuthController {
         this.jwtProvider = jwtProvider;
         this.customerUserDetailsService = customerUserDetailsService;
         this.cartRepository = cartRepository;
+
     }
+
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
+
+        User isEmailExist = userRepository.findByEmail(user.getEmail());
+
+        if (isEmailExist != null) {
+            throw new Exception("Email is already used with another account");
+        }
+
+        User createdUser = new User();
+        createdUser.setEmail(user.getEmail());
+        createdUser.setFullName(user.getFullName());
+        createdUser.setRole(user.getRole());
+        createdUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User savedUser = userRepository.save(createdUser);
+
+        Cart cart = new Cart();
+        cart.setCustomer(savedUser);
+        cartRepository.save(cart);
+
+        return null;
+
+    }
+
 }
---
+++ src/main/java/com/anuwat/food_order_online/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.anuwat.food_order_online.response;
+
+import com.anuwat.food_order_online.model.USER_ROLE;
+
+public class AuthResponse {
+
+    private String jwt;
+
+    private String message;
+
+    private USER_ROLE role;
+
+}
