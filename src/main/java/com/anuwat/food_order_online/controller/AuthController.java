863556418a75f8d0757b0df7e47bc15caf7ca969
anuwatdev245 <tox.anw.6114@gmail.com>
2025-05-08 17:26:55 +0700

    commit

--- src/main/java/com/anuwat/food_order_online/controller/AuthController.java
+++ src/main/java/com/anuwat/food_order_online/controller/AuthController.java
@@ -7,8 +7,13 @@ import com.anuwat.food_order_online.repository.CartRepository;
 import com.anuwat.food_order_online.repository.UserRepository;
 import com.anuwat.food_order_online.response.AuthResponse;
 import com.anuwat.food_order_online.service.CustomerUserDetailsService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +37,7 @@ public class AuthController {
 
     }
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
 
         User isEmailExist = userRepository.findByEmail(user.getEmail());
@@ -52,7 +58,16 @@ public class AuthController {
         cart.setCustomer(savedUser);
         cartRepository.save(cart);
 
-        return null;
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register success");
+        authResponse.setRole(savedUser.getRole());
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
 
     }
 
--- src/main/java/com/anuwat/food_order_online/response/AuthResponse.java
+++ src/main/java/com/anuwat/food_order_online/response/AuthResponse.java
@@ -1,7 +1,9 @@
 package com.anuwat.food_order_online.response;
 
 import com.anuwat.food_order_online.model.USER_ROLE;
+import lombok.Data;
 
+@Data
 public class AuthResponse {
 
     private String jwt;
